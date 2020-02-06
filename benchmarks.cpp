abc6f47e7781a161bd3c220b2a2d1a2c91a56e11
Solomon Lucas <soluke22@gmail.com>
2020-02-05 22:14:20 -0500

    adding stuff and fixing stuff

--- benchmarks.cpp
+++ benchmarks.cpp
@@ -6,7 +6,7 @@ using namespace std;
     void integer_benchmark(){
         time_t start, end; 
         time(&start); 
-        long long sum;
+        long sum;
         for(long i = 0; i < 100000000001;i++){
           sum = 1+1;
         }         
@@ -18,7 +18,7 @@ using namespace std;
         }
         time(&end); 
         double time_taken = double(end - start);
-        cout << "The time taken for the integer benchmark is  " << time_taken <<  "seconds" << endl;
+        cout << "The time taken for the integer benchmark is  " << time_taken <<  " seconds" << endl;
     }
 
     void floating_point_benchmark(){
@@ -36,31 +36,42 @@ using namespace std;
         }
         time(&end); 
         double time_taken = double(end - start);
-        cout << "The time taken for the floating benchmark is  " << time_taken <<  "seconds" << endl;
+        cout << "The time taken for the floating benchmark is  " << time_taken <<  " seconds" << endl;
     }
 
     void memory_benchmark(){
         time_t start, end; 
         time(&start);
-        int a[5000000000];
-        int b[5000000000];
+        long long a[5000000000];
+        long long b[5000000000];
         for(long long i = 0; i < 5000000001; i++){
             a[i] = 2;
         }
         for(long long i = 0; i < 5000000001; i++){
-            a[i] = a[i];
+            b[i] = a[i];
         }
         time(&end);
         double time_taken = double(end - start);
-        cout << "The time taken for the memory benchmark is  " << time_taken <<  "seconds" << endl;
+        cout << "The time taken for the memory benchmark is  " << time_taken <<  " seconds" << endl;
+    }
+
+    void hard_drive_benchmark(){
+        ofstream file;
+        file.open("filetoopen.txt");
+        for(long long i; i < 10; i++){
+            file << i << endl;
+        }
+        file.close();
+
     }
 
 
 int main(){
 
-    integer_benchmark();
-    floating_point_benchmark();
-    memory_benchmark();
+    //integer_benchmark();
+    //floating_point_benchmark();
+    //memory_benchmark();
+    hard_drive_benchmark();
     return 0;
 
 }
