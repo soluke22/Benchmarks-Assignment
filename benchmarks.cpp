9c2dbd17c0ad62eccbf86fee6e8a3fcd3e751779
Solomon Lucas <soluke22@gmail.com>
2020-02-06 12:48:11 -0500

    adding comments

--- benchmarks.cpp
+++ benchmarks.cpp
@@ -42,11 +42,11 @@ using namespace std;
     void memory_benchmark(){
         time_t start, end; 
         time(&start);
-        long double a[10000000];
-        long double b[10000000];
+        long double a[10000000];//this array is causing segment faults
+        long double b[10000000];//so is this one
         for(int i = 0; i < 60; i++){
         for(long  i = 0; i < 10000001; i++){
-            a[i] = i;
+            a[i] = 1;
         }
         for(long i = 0; i < 100000001; i++){
             b[i] = a[i];
