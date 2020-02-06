7b23504aa3a3be94c6b0efb83f684b7a163b675c
Solomon Lucas <soluke22@gmail.com>
2020-02-05 19:37:33 -0500

    fixed spacing; added timing inside the function

--- benchmarks.cpp
+++ benchmarks.cpp
@@ -7,19 +7,24 @@ class benchmarks
 {
     public: 
     void integer_benchmark(){
-      long long sum;
-      sum = 0;
-      for(long i = 0; i < 100000000000;i++){
-      sum += 1;
-      }         
-      sum = 1;
-      for(long i = 0; i < 50000000000;i++){
-        sum *= 2;
-      }
-      sum = 100000000002;
-      for(long i = 0; i < 20000000000;i++){
-        sum /= 2;
-      }
+        time_t start, end; 
+        time(&start); 
+        long long sum;
+        sum = 0;
+        for(long i = 0; i < 100000000000;i++){
+        sum += 1;
+        }         
+        sum = 1;
+        for(long i = 0; i < 50000000000;i++){
+            sum *= 2;
+        }
+        sum = 100000000002;
+        for(long i = 0; i < 20000000000;i++){
+            sum /= 2;
+        }
+        time(&end); 
+        double time_taken = double(end - start);
+        cout << "The time taken for the integer benchmark is  " << time_taken << endl;
     }
 
     void floating_point_benchmark(){
@@ -42,8 +47,7 @@ class benchmarks
 int main(){
 
     benchmarks test;
-    time_t start, end; 
-    time(&start); 
+    
     test.integer_benchmark();
     time(&end); 
     double time_taken = double(end - start);
